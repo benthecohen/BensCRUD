ec4e2c8621761a3e7118f638829e65a470136417
benthecohen <benthecohen@gmail.com>
2013-12-16 05:32:24 -0500

    replaced missing line (session_start();)

--- logincontroller.php
+++ logincontroller.php
@@ -3,6 +3,7 @@ include("usermodel.php");
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	if (User::authenticateUser($_POST["username"], $_POST["password"])) {
+		session_start();
 		$_SESSION["username"] = $_POST["username"];
 		echo '<script type="text/javascript">
 			window.location = "dashboard.php"; 
