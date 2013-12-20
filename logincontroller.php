c3d3bdc1a46fa5e1c2ff3d003f178614a30aad8a
benthecohen <benthecohen@gmail.com>
2013-12-20 04:22:44 -0500

    Minor Formatting

--- logincontroller.php
+++ logincontroller.php
@@ -12,6 +12,4 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 		echo 'Wrong username and/or password.';
 	}
 }
-
-
 ?>
--- newuser.php
+++ newuser.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 
-<?php include("newusercontroller.php");
-?>
+<?php include("newusercontroller.php");?>
 <head>
 	<style>
 	input {
@@ -12,12 +11,13 @@
 </head>
 
 <body>
-<h1>Register New User Account</h1>
-<form action="newuser.php" method="post">
-<input type="text" name="username" placeholder="Your Username" />
-<input type="password" name="password" placeholder="Your Password" />
-<input type="text" name="firstname" placeholder="Your First Name" />
-<input type="text" name="lastname" placeholder="Your Last Name" />
-<input type="email" name="email" placeholder="Your Email" />
-<input type="submit" value="Submit" />
+	<h1>Register New User Account</h1>
+	<form action="newuser.php" method="post">
+	<input type="text" name="username" placeholder="Your Username" />
+	<input type="password" name="password" placeholder="Your Password" />
+	<input type="text" name="firstname" placeholder="Your First Name" />
+	<input type="text" name="lastname" placeholder="Your Last Name" />
+	<input type="email" name="email" placeholder="Your Email" />
+	<input type="submit" value="Submit" />
+	</form>
 </body>
